ca1e4728c260362bdaf22188ae97355b38534744
xu jinshui <xujinshui@bcadr.cn>
2021-12-10 10:36:19 +0800

    add ex3_7 ex3_8

--- ex3_8/index.js
+++ ex3_8/index.js
@@ -1,6 +1,11 @@
 const express=require('express')
+const morgan=require('morgan')
 const app=express()
 app.use(express.json())
+
+morgan.token('postvalue',req=>JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postvalue',{skip:(req)=> req.method != 'POST'}))
+app.use(morgan('tiny'))
 
 let phonebooks=
 [
